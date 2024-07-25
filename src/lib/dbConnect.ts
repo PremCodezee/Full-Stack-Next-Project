917dd9d46adf026c1a36ae5b92b09589a750edac
Prem Codezee <prem.codezee.gmail.com>
2024-07-25 16:28:39 +0530

    Database Connection

---
+++ src/lib/dbConnect.ts
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+type connectionObject = {
+  isConnected?: number;
+};
+
+const connection: connectionObject = {};
+
+async function dbConnect(): Promise<void> {
+  if (connection.isConnected) {
+    console.log("Already connected to Database");
+    return;
+  }
+
+  try {
+    const db = await mongoose.connect(process.env.MONGO_URI || "", {});
+
+    connection.isConnected = db.connections[0].readyState;
+
+    console.log("Connected to Database Successfully");
+  } catch (error) {
+    console.log("Database Connection Failed" , error);
+    process.exit(1)
+  }
+}
+
+export default dbConnect;
